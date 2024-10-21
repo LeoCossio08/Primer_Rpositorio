8fe4a9ca17e9ba8ff61520fd6ca5910a0b3c7697
Leo_Cossio08 <joseleonardocossiorodriguez@gmail.com>
2024-10-21 09:10:08 -0600

    Add files via upload

---
+++ P01variables1.php
@@ -0,0 +1,18 @@
+<?php 
+$dia=24; //se declara una variable de tipo intenger.
+$sueldo=758.43; //se declara una variable tipo double
+$nombre= "Juan"; //se declara una variable de tipo string.
+$exite = true; // se declara una variable boolean .
+echo "variable entera:";
+echo $dia;echo "<br>";
+echo "variable double:";
+echo $sueldo; echo "<br>";
+echo "variable string";
+echo $nombre; echo "<br>";
+echo "variable boolean";
+echo $exite;
+echo "<br>";
+$cadena1="Hola";
+$cadena2="Mundo";
+echo $cadena1. " ".$cadena2;
+?>
