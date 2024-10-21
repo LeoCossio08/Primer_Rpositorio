f3d549d1705e567581e32e42ecdfc91534ce67c4
Leo_Cossio08 <joseleonardocossiorodriguez@gmail.com>
2024-10-21 09:30:53 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,13 @@
+<?php
+/*
+CBTIS 89
+Leonardo Cossio
+Programacion 3°A
+*/
+echo " Mi Nombre es: Jose Leonardo Cossio Rodriguez";
+echo "<br>";
+echo "Soy de Dgo. Durango";
+echo "<br>";
+echo "Estudio en el CBTIS 89 ";
+echo  "<br>";
+echo "Estudio la carrera de Programacion en 3°A T.M";
